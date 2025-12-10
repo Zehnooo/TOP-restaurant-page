31e45bd10df13110413023bec0003cd97e269ead
Zehno <126801501+Zehnooo@users.noreply.github.com>
2025-12-10 16:58:29 -0600

    initial project commit -- started hero section to make sure html, js, and css will work with webpack

---
+++ src/builder.js
@@ -0,0 +1,30 @@
+import cover from './assets/coverimg.jpg';
+
+export function homePage(){
+    const content = document.querySelector('#content');
+
+    const container = document.createElement('div');
+
+    const imageContainer = document.createElement('div');
+        imageContainer.id = 'imgCont';
+
+    const image = document.createElement('img');
+        image.src = cover;
+
+    const imgText = document.createElement('div');
+    imgText.id = 'imgText';
+
+    const mainHeading = document.createElement('h3')
+        mainHeading.textContent = 'Welcome to';
+    const name = document.createElement('h1')
+        name.textContent = "Zehno's Restaurant";
+        name.style.fontStyle = 'italic';
+    const secondaryHeading = document.createElement('h3')
+        secondaryHeading.textContent = 'Modern, beautiful, delicious.'
+
+    imgText.append(mainHeading, name, secondaryHeading)
+
+    imageContainer.append(imgText, image);
+    container.append(imageContainer);
+    content.append(container);
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './styles.css'
+import {homePage} from './builder.js'
+
+
+console.log("hello wall");
+homePage();
