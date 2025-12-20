a61189d7b9ced2eebb74b05ff51a8b6ae0e6ee5a
Zehno @ Home <126801501+Zehnooo@users.noreply.github.com>
2025-12-19 22:08:24 -0600

    added header and button variables, added classes to many elements

--- src/builder.js
+++ src/builder.js
@@ -1,25 +1,36 @@
 import cover from './assets/coverimg.jpg';
-import logo from './assets/logo-small.jpg';
+import logo from './assets/logo-small.png';
 
 export function buildHomePage(){
     const content = document.querySelector('#content');
 
+    const headContainer = document.querySelector('#head-container');
     const logoContainer = document.querySelector('#logo-container');
     const logoImg = document.createElement('img');
     logoImg.src = logo;
+    document.querySelector('header').style.backgroundColor = 'white';
+    const name = document.createElement('p');
+    name.textContent = 'Zehno\'s Ramen';
+    name.style.color = 'black';
     logoContainer.append(logoImg);
+    headContainer.append(logoContainer, name);
 
+    const header = document.querySelector("header");
+    const buttons = header.querySelectorAll('button');
+    buttons.forEach(button => button.classList.add('hvr-grow'));
+    buttons.forEach(button => button.classList.add('hvr-bubble-bottom'));
+    buttons.forEach(button => button.classList.add('hvr-underline-from-center'));
     const container = document.createElement('div');
 
     const imageContainer = document.createElement('div');
         imageContainer.id = 'imgCont';
 
     const image = document.createElement('img');
-        image.src = cover;
-
+    image.src = cover;
+    image.classList.add('hidden');
     const imgText = document.createElement('div');
     imgText.id = 'imgText';
-
+/*
     const mainHeading = document.createElement('h3')
         mainHeading.textContent = 'Welcome to';
     const name = document.createElement('h1')
@@ -29,16 +40,29 @@ export function buildHomePage(){
 
     imgText.append(mainHeading, name, secondaryHeading)
 
-    imageContainer.append(imgText, image);
+
+
+ */
+
+    imageContainer.append(image);
     container.append(imageContainer);
     content.append(container);
 
+
     if (image.complete){
-        document.body.classList.add('reveal');
+   // image.classList.remove('hidden');
+        image.classList.add('fade-in');
+        logoImg.classList.add('tracking-in');
+        name.classList.add('tracking-in');
     } else {
         image.addEventListener('load', () => {
-            document.body.classList.add('reveal');
+         //   image.classList.remove('hidden');
+        setTimeout(() => image.classList.add('fade-in'), 500);
         });
+        logoImg.classList.add('tracking-in');
+        name.classList.add('tracking-in');
     }
 
+    document.body.classList.add('reveal');
+    document.body.classList.remove('hidden');
 }
