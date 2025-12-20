20fd13041cea27cb91001bb7ebbfe9ce9906d7b6
Zehno @ Home <126801501+Zehnooo@users.noreply.github.com>
2025-12-20 01:50:07 -0600

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
+        publicPath: '/TOP-restaurant-page/',
         clean: true,
     },
     plugins: [
